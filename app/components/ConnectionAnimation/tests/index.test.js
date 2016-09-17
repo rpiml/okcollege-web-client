39892af327b1b443f5a778c9d8c979aee143b685
seveibar <seveibar@gmail.com>
2016-09-17 05:42:19 -0400

    landing page header

--- app/components/ConnectionAnimation/tests/index.test.js
+++ app/components/ConnectionAnimation/tests/index.test.js
@@ -1,10 +1,10 @@
-// import PageHero from '../index';
+// import CanvasDrawing from '../index';
 
 import expect from 'expect';
 // import { shallow } from 'enzyme';
 // import React from 'react';
 
-describe('<PageHero />', () => {
+describe('<ConnectionAnimation />', () => {
   it('Expect to have unit tests specified', () => {
     expect(true).toEqual(false);
   });
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -13,8 +13,7 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import PageHeader from 'components/PageHeader/';
-import PageHero from 'components/PageHero';
+import ConnectionAnimation from 'components/ConnectionAnimation';
 import styles from './styles.css';
 
 export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
@@ -22,17 +21,17 @@ export default class HomePage extends React.Component { // eslint-disable-line r
   render() {
     return (
       <div>
-        <PageHeader />
-        <Grid>
-          <Row>
-            <Col xs={1}>
-              {"Yoo1"}
-            </Col>
-            <Col xs={1}>
-              {"Yooo"}
+        <ConnectionAnimation height={200} />
+        <div className={styles.fgShift}>
+          <Row center="xs">
+            <Col xs={12} md={10} lg={9}>
+              <div className={styles.logo}> OkCollege </div>
+              <div className={styles.tagLine}>
+                college discovery with machine learning
+              </div>
             </Col>
           </Row>
-        </Grid>
+        </div>
       </div>
     );
   }
