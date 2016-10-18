80a54ec5c6703083e8ca6295a41dd7c976ffd770
jowlee <joeylee0925@gmail.com>
2016-10-18 15:31:24 -0400

    dropdown component

---
+++ app/components/MultiChoiceDropdown/tests/index.test.js
@@ -0,0 +1,11 @@
+// import MultiChoiceDropdown from '../index';
+
+import expect from 'expect';
+// import { shallow } from 'enzyme';
+// import React from 'react';
+
+describe('<MultiChoiceDropdown />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
