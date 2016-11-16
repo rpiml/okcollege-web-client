f966ab6518ba867a778f325ff75d3c1c69af6741
seveibar <seveibar@gmail.com>
2016-11-15 21:21:20 -0500

    added loading action and styling

--- app/containers/ResultsPage/index.js
+++ app/containers/ResultsPage/index.js
@@ -9,9 +9,32 @@ import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import selectResultsPage from './selectors';
 import styles from './styles.css';
+import {Row, Col,Container} from 'reactstrap';
 
 export class ResultsPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
+
+    const { hasResults, rankings } = this.props;
+
+    const rankingElements = rankings.map(college => {
+      return (
+        <Row className={styles.rankingElement}>
+          <Col xs={{size:2}} className={styles.collegeRanking}>
+            <div> {college.ranking} </div>
+          </Col>
+          <Col xs={{size:10}} className={styles.collegeName}>
+            {college.name}
+          </Col>
+        </Row>
+      );
+    });
+
+    const loadingElement = (
+      <div>
+      Loading
+      </div>
+    );
+
     return (
       <div className={styles.resultsPage}>
       <Helmet
@@ -20,6 +43,9 @@ export class ResultsPage extends React.Component { // eslint-disable-line react/
           { name: 'description', content: 'Description of ResultsPage' },
         ]}
       />
+      <Container>
+        { hasResults ? rankingElements : loadingElement }
+      </Container>
       </div>
     );
   }
--- app/containers/ResultsPage/reducer.js
+++ app/containers/ResultsPage/reducer.js
@@ -9,7 +9,23 @@ import {
   DEFAULT_ACTION,
 } from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  hasResults: true,
+  rankings: [
+    {
+      name: "Rensselaer Polytechnic Institute",
+      ranking: 1
+    },
+    {
+      name: "Massachusetts Institute of Technology",
+      ranking: 2
+    },
+    {
+      name: "Stanford University",
+      ranking: 3
+    }
+  ]
+});
 
 function resultsPageReducer(state = initialState, action) {
   switch (action.type) {
