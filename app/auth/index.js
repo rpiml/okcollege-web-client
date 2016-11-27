87aeb75c3ab78abbb96f8785abb520731a7f6cad
Severin Ibarluzea <seve700@gmail.com>
2016-11-27 01:26:25 -0500

    fixed typo

--- app/auth/index.js
+++ app/auth/index.js
@@ -2,5 +2,5 @@ export function authFetch(path, params){
   params = params || {};
   let request = new Request('path', params);
   request.set('authorization', 'Bearer ' + token);
-  return fetch(request);s
+  return fetch(request);
 }
