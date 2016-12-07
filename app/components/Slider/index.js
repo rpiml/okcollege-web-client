bada98d3904182d44c50f1d66ede2aea37b14168
Andrew Kaiser <kaisea@rpi.edu>
2016-12-06 21:33:53 -0500

    answering turns background red

--- app/components/Slider/index.js
+++ app/components/Slider/index.js
@@ -1,57 +1,52 @@
 /**
-*
-* Slider
-*
-*/
+ *
+ * Slider
+ *
+ */
 
 import React from 'react';
-
+import { Slider as Slider_antd } from 'antd';
 import { FormattedMessage } from 'react-intl';
+
 import messages from './messages';
 import styles from './styles.css';
-// import InputRange from 'react-input-range';
-import { Slider as Slider_antd } from 'antd';
 
-// require("!style!css!react-input-range/dist/react-input-range.css");
 require("!style!css!antd/lib/slider/style/index.css");
 require("!style!css!antd/lib/tooltip/style/index.css");
 
 
-class Slider extends React.Component { // eslint-disable-line react/prefer-stateless-function
-
-  componentWillMount() {
-    if(this.props.answer === undefined){
-      this.props.onChange((this.props.range[0] + this.props.range[1])/2);
-    }
+let removeTrailingZeroes = (v) => {
+  if (v.toString().split('').filter(c => c == '0').length > 8){
+    return parseFloat(v.toString().slice(0, v.toString().length - 2));
+  } else {
+    return v;
   }
+}
 
-  removeTrailingZeroes(v){
-    if (v.toString().split('').filter(c => c == '0').length > 8){
-      return parseFloat(v.toString().slice(0, v.toString().length - 2));
-    }else{
-      return v;
-    }
-  }
+const Slider = (props) => {
 
-  render() {
-    let marks = {};
-    marks[this.props.range[0]] = this.props.range[0].toString();
-    marks[this.props.range[1]] = this.props.range[1].toString();
+  let marks = {};
+  marks[props.range[0]] = props.range[0].toString();
+  marks[props.range[1]] = props.range[1].toString();
 
-    return (
-      <div className={styles.slider}>
-        <div className={styles.questionText}>{this.props.question}</div>
-        <Slider_antd
-            marks={marks}
-            max={this.props.range[1]}
-            min={this.props.range[0]}
-            step={this.props.step || 1}
-            formatLabel={this.removeTrailingZeroes}
-            onAfterChange={this.props.onChange}
-            />
-      </div>
-    );
-  }
+  const defaultValue = (props.range[0] + props.range[1])/2
+
+
+
+  return (
+    <div className={styles.slider}>
+      <div className={styles.questionText}>{props.question}</div>
+      <Slider_antd
+        defaultValue={props.answer || defaultValue}
+        marks={marks}
+        max={props.range[1]}
+        min={props.range[0]}
+        step={props.step || 1}
+        formatLabel={removeTrailingZeroes}
+        onAfterChange={props.onChange}
+      />
+    </div>
+  );
 }
 
 Slider.propTypes = {
