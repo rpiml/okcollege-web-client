76f647b69cb8bf04128d748641f06cc7a7f4e607
seveibar <seveibar@gmail.com>
2016-12-07 19:55:51 -0500

    removed test

--- app/components/Text/tests/index.test.js
+++ app/components/Text/tests/index.test.js
@@ -5,7 +5,4 @@ import expect from 'expect';
 // import React from 'react';
 
 describe('<Text />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
 });
